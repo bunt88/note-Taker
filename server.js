e404e62b0a762df95aa7bb669cdae2ca30ce170e
unknown <bunt88@gmail.com>
2022-09-13 10:42:36 -0600

    Added html routes

--- server.js
+++ server.js
@@ -16,18 +16,6 @@ app.get('/api/notes', (req, res) => {
     res.json(allNotes.slice(1));
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-});
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
 function createNewNote(body, notesArray) {
     const newNote = body;
     if (!Array.isArray(notesArray))
